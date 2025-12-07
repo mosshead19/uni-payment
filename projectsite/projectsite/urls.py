e1cb59fe98a9c8e1d0de628bbf264030a11bcb2e
John Christopher Remandaban <202380127@psu.palawan.edu.ph>
2025-12-07 12:36:55 +0800

    Add PWA support (settings, URLs, template meta, service worker)

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -27,6 +27,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('pwa.urls')),
       path('accounts/', include('allauth.urls')),
         
 
